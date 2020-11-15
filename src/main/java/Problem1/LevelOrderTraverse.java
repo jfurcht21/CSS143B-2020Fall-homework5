1314ee4746340dcefcca3b621a353a4b67ec7f4c
John <jfurcht21@gmail.com>
2020-11-15 11:58:55 -0800

    First commit

--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -1,12 +1,31 @@
 package Problem1;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
-        // homework
+
         List<List<Integer>> result = new ArrayList<>();
-        return result;  // place holder
+        Queue<TreeNode> newQueue = new ArrayDeque<>();
+        List<Integer> addResult = new ArrayList<>();
+
+        newQueue.add(root);
+        newQueue.add(null);
+
+        while(newQueue.size() > 0){
+           root = newQueue.poll();
+
+           if(root == null){
+               if(newQueue.size() == 0){
+                   newQueue.add(null);
+
+               }
+           }
+
+        }
+        return result;
     }
 }
--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -1,9 +1,59 @@
 package Problem3;
 
 import Problem1.TreeNode;
-
+// Reference when I hit a blocker https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+// Needed to use a helper function that returned a TreeNode
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
-        // homework
+        insertHelper(root,valToInsert);
     }
+
+    public static TreeNode insertHelper(TreeNode root, int valToInsert){
+        if(root == null){
+            root = new TreeNode<>(valToInsert);
+            return root;
+        }
+        if(valToInsert < (int) root.val){
+            root.left = insertHelper(root.left,valToInsert);
+        }
+        else{
+            root.right = insertHelper(root.right,valToInsert);
+        }
+        return root;
+    }
+    // Code that wasn't working directly in insert()
+        /*if(valToInsert > root.val){
+            if(root.right == null){
+                TreeNode insert = new TreeNode<>(valToInsert);
+                root.right = insert;
+                return;
+            }
+            else {
+                root = root.right;
+                insert(root, valToInsert);
+            }
+        }
+        if(valToInsert < root.val || valToInsert == root.val){
+            if(root.left == null){
+                TreeNode insert = new TreeNode<>(valToInsert);
+                root.left = insert;
+                return;
+            }
+            else{
+                root = root.left;
+                insert(root, valToInsert);
+            }
+        }*/
+        /*
+        if(root == null){
+            root = new TreeNode<>(valToInsert);
+            return;
+        }
+        if(valToInsert < root.val){
+            insert(root.left,valToInsert);
+        }
+        else{
+            insert(root.right,valToInsert);
+        }*/
+
 }
