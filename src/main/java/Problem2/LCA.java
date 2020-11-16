d322bf80da20a888f4e747f991579907d524e208
John <jfurcht21@gmail.com>
2020-11-15 17:41:47 -0800

    Updates problem 2

--- src/main/java/Problem2/LCA.java
+++ src/main/java/Problem2/LCA.java
@@ -4,8 +4,24 @@ import Problem1.TreeNode;
 
 public class LCA {
     public static TreeNode<Integer> lowestCommonAncestor(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) {
-        // homework
-        // hint: pay attention to the assumptions of this problem.
-        return null;    // place holder
+           if (root == null)
+                return null;
+
+            if(root.val == p.val || root.val == q.val){
+                return root;
+            }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left != null && right != null){
+            return root;
+        }
+        if(left == null){
+            return right;
+        }
+        else {
+            return left;
+        }
     }
 }
